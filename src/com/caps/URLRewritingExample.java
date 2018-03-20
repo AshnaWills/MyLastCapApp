20e369b3105457ed6a697b6016055a13bde40352
creatorHead <droidbit1@gmail.com>
2018-03-20 21:27:13 +0530

    Added URLRewritingExample.java and Time.jsp

--- src/com/caps/URLRewritingExample.java
+++ src/com/caps/URLRewritingExample.java
@@ -1,20 +1,24 @@
 package com.caps;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Date;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-public class MyFirstServlet extends HttpServlet {
+@WebServlet("/urlRewriting")
+public class URLRewritingExample extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String s = null;
-		resp.setContentType("text/html");
-		PrintWriter out = resp.getWriter();
-		out.print("<h1>The time is: "+new Date()+"</h1>");
+
+		HttpSession session = req.getSession();
+		
+		String encodedUrl = resp.encodeURL("./time");
+		System.out.println(encodedUrl);
+		
 	}
 }
+
